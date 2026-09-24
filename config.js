13013af0319aab39528489fe9c4e94c921dff49f
piknik1979 <tpikniczka@gmail.com>
2026-09-24 11:01:38 +0100

    Leaderboard sorted

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  SUPABASE_URL: "https://icodelhzrzmdlrpgsygh.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://icodelhzrzmdlrpgsygh.supabase.co/",
   SUPABASE_KEY:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imljb2RlbGh6cnptZGxycGdzeWdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyNTM2OTIsImV4cCI6MjA5MTgyOTY5Mn0.1CIOeHwKxKMBwX2Lee9OA0LYFpVdDevealH6l6PlkC4",
   SUPABASE_TABLE: "pub_quiz_scores",
